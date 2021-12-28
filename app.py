5a31491651beb38b30f7a464e8e8db3ac6b23624
Anushka Kansal <anushkakkansal@gmail.com>
2021-12-28 08:34:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ number1 = st.slider("pick a number", 0, 100)
 number2 = st.slider("Pick another number", 0, 100)
 x = number1+number2
 y = number1-number2
-st.title("sum: ", x)
-st.title("difference: ", y)
+st.title("SUM: ")
+st.write(x)
+st.title("DIFFERENCE: ")
+st.write(y)
   
